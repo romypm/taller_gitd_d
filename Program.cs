5fc310bbabe25e88cb84edd0749541b0ad3502cf
romypm <pmr1010708@est.univalle.edu>
2023-05-10 16:05:21 -0400

    primer commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int result = sum(2, 3);
+        }
+        public int sum (int n1, int n2) 
+        {
+            return n1 + n2;
+        }
+    }
+}
